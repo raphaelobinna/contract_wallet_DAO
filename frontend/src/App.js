2920fb6b1699fc5a46379741fdc8cbf1c8baf016
Raphael Ogbonnaya <raphaelogbonnaya@gmail.com>
2021-11-04 21:07:34 +0200

    frontend initialization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
+import Tables from './pages/Tables';
+import Footer from './components/Footer';
+
+// Font Awesome Style Sheet
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// Tailwind CSS Style Sheet
+import './assets/styles/tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <Sidebar />
+          <div className="md:ml-64">
+              <Switch>
+                  <Route exact path="/" component={Dashboard} />
+                  <Route exact path="/tables" component={Tables} />
+              </Switch>
+              <Footer />
+          </div>
+      </>
   );
 }
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['"Roboto Slab"', 'serif'],
+        body: ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
